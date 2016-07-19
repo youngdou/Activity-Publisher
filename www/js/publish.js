ffe08406bdafc0783963e8712d8b6adf4453dd00
youngdou <yaoyongdou@gmail.com>
2016-07-19 11:55:35 +0800

    the first debug from feedback

--- www/js/publish.js
+++ www/js/publish.js
@@ -1,108 +1,29 @@
 var isCheckPass = {
-	"#actName" : false,
-	"#actTime" : false,
-	"#actLoc" : false,
-	"#actIntru" : false,
-	"#actFor" : false,
-	"#actPub" : false,
-	"#actJoin" : false,
-	"#actDem" : true
+	"#actName" 	: 	false,
+	"#actTime" 	: 	false,
+	"#actLoc" 	: 	false,
+	"#actIntru" : 	false,
+	"#actFor" 	: 	false,
+	"#actPub" 	: 	false,
+	"#actJoin" 	: 	false,
+	"#actDDL" 	: 	false,
+	"#actDetail": 	false,
+	"#actReward" : 	true,
+	"#actDem" 	: 	true
 }
 
 $(document).ready(function() {
 
 	//initial the time picker
-	$("#time").datetimePicker({
+	$("#actDDL").datetimePicker({
 	  min: "2012-12-12",
-	  max: "2022-12-12 12:12"
+	  max: "2022-12-12 12:12",
 	});
 
-	//绑定字数函数
+	//绑定检验函数
+	bindCheck();
 
-	//活动名称
-	$("#actName").keyup(function() {
-		checkCounter("#actName", 2);
-	});
-	$("#actName").blur(function() {
-		checkCounterOk("#actName");
-	})
-	// 时间
-
-    $("#actTime").keyup(function() {
-    	checkCounter("#actTime", 100);
-    });
-    $("#actTime").blur(function() {
-    	checkCounterOk("#actTime");
-    })
-
-    // 地点actLoc
-
-    $("#actLoc").keyup(function() {
-    	checkCounter("#actLoc", 100);
-    });
-    $("#actLoc").blur(function() {
-    	checkCounterOk("#actLoc");
-    })
-
-    // 活动简介
-
-    $("#actIntru").keyup(function() {
-    	checkCounter("#actIntru", 50);
-    });
-    $("#actIntru").blur(function() {
-    	checkCounterOk("#actIntru");
-    })
-
-    //活动对象
-
-    $("#actFor").keyup(function() {
-    	checkCounter("#actFor", 30);
-    });
-    $("#actFor").blur(function() {
-    	checkCounterOk("#actFor");
-    })
-
-    //主办方
-
-    $("#actPub").keyup(function() {
-    	checkCounter("#actPub", 50);
-    });
-    $("#actPub").blur(function() {
-    	checkCounterOk("#actPub");
-    })
-
-    //报名方式
-
-    $("#actJoin").keyup(function() {
-    	checkCounter("#actJoin", 200);
-    });
-    $("#actJoin").blur(function() {
-    	checkCounterOk("#actJoin");
-    })
-
-    //活动详情
-	$("#actDetail").keyup(function() {
-		checkCounter("#actDetail", 200);
-	});
-	$("#actDetail").blur(function() {
-		checkCounterOk("#actDetail");
-	})
-
-	//活动要求
-
-	$("#actDem").keyup(function() {
-		checkCounter("#actDem", 100);
-	});
-	$("#actDem").blur(function() {
-		checkCounterOk("#actDem");
-	})
-
-	//发布内容
-	$(document).on('click', '#public_button', function() {
-        checkNeceValid();
-    })
 })
-
 // 检查字数函数
 // argument: selector <string>; numOfWords <interge>
 // return:null <null>
@@ -111,41 +32,130 @@ function checkCounter(selector, numOfWords) {
 	var len = text.length;
 	$(selector+"_counter").text(len+"");
 
-	isCheckPass[selector] = true;
 	if (len > numOfWords) {
-		$(selector+"_showTips i").attr("class", "weui_icon_warn");
+		showFaild(selector);
 
-		$(selector+"_showTips").fadeIn(150);
-		$(selector+"_showTips .warningTips").fadeIn(150);
 		$(selector).val($(selector).val().substring(0,numOfWords+1));
 		$(selector+"_counter").text(numOfWords+1+"");
 
 		isCheckPass[selector] = false;
-	} else {
+	} 
+	else if (len > 0) {
+		isCheckPass[selector] = true;
 		$(selector+"_showTips").fadeOut();
+	} else {
+		isCheckPass[selector] = false;
 	}
 }
 
 function checkNeceValid() {
-	var isValid = false;
+	var validNum = 0;
+	var isCheckPassLength = 0;
 	for (var key in isCheckPass) {
-		isValid = isCheckPass[key];
+		if (isCheckPass[key]) {
+			validNum++;
+		}
+		isCheckPassLength++;
+		// console.log(key+" "+ "\tNum: "+validNum + "\tisPassLength: "+isCheckPassLength);
 	}
 
-	if (isValid == false) {
+	if (validNum != isCheckPassLength) {
 		//提示格式错误
-		$(document).on('click', '#public_button', function() {
-	        $.toptip('格式有点问题，回头看看哪里错了ఠ_ఠ', 'error');
-	    })
+        $.toptip('格式好像有错/漏ఠ_ఠ', 'error');
 	} else {
 		//发布信息
 		$.showLoading("发布中");
 	}
 }
 function checkCounterOk(selector) {
-	if (isCheckPass[selector] == true) {
-		$(selector+"_showTips i").attr("class", "weui_icon_success");
-		$(selector+"_showTips span").hide();
-		$(selector+"_showTips").fadeIn(150);
+	if (isCheckPass[selector] == true ) {
+		showSuccess(selector);
+	} else {
+		$(selector+"_showTips").hide();
 	}
+}
+
+function checkMutiItem(numOfWords) {
+	var len_pe = $("#PEChapter").val().length;
+	var len_wel = $("#welTime").val().length;
+	var len_other = $("#other").val().length;
+
+	if ((len_other + len_wel + len_pe) == 0) {
+		$("#actReward_showTips").fadeOut(150);
+	} 
+	else if (len_pe > numOfWords || len_wel > numOfWords || len_other > numOfWords) {
+		isCheckPass["#actReward"] = false;
+		//show tips
+		showFaild("#actReward");
+	} else {
+		showSuccess("#actReward")
+	}
+}
+function bindCheck() {
+	//用json对象进行每个输入框的设置（字数长度设置）
+	var options = {
+		"#actName" 		: 	30,
+		"#actTime" 		: 	100,
+		"#actLoc"		: 	100,
+		"#actIntru" 	: 	50,
+		"#actFor" 		: 	30,
+		"#actPub" 		: 	50,
+		"#actJoin" 		: 	200,
+		"#actDetail" 	: 	2,
+		"#actDem" 		: 	100
+	}
+
+	//绑定 字数函数
+	for (var key in options) {
+		bindCheckOne(key, options[key]);
+	}
+	//绑定 活动奖励
+	CheckActReward();
+	//绑定 截止时间
+	$("#actDDL").click(function() {
+		checkCounter("#actDDL", 16);
+		checkCounterOk("#actDDL");
+		console.log("#actDDL" + 16);
+	});
+	//绑定 发布内容
+	$(document).on('click', '#public_button', function() {
+        checkNeceValid();
+    });
+}
+
+function bindCheckOne(selector, numOfWords) {
+	$(selector).keyup(function() {
+		checkCounter(selector, numOfWords);
+	});
+	$(selector).blur(function() {
+		checkCounterOk(selector);
+	})
+}
+function CheckActReward() {
+	//体育章
+	checkOneReward("#PEChapter", 2);
+
+	//公益时
+	checkOneReward("#welTime", 2);
+
+	//其他奖励
+	checkOneReward("#other", 2);
+}
+function checkOneReward(selector, numOfWords) {
+	$(selector).keyup(function() {
+		checkCounter(selector, numOfWords);
+	});
+	$(selector).blur(function() {
+		checkMutiItem(numOfWords);
+	})
+}
+function showSuccess(selector) {
+	$(selector+"_showTips i").attr("class", "weui_icon_success");
+	$(selector+"_showTips span").hide();
+	$(selector+"_showTips").fadeIn(150);
+}
+function showFaild(selector) {
+	$(selector+"_showTips i").attr("class", "weui_icon_warn");
+	$(selector+"_showTips").fadeIn(150);
+	$(selector+"_showTips .warningTips").fadeIn(150);
 }
